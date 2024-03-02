815eab25c9406f3bf62dedee0f2f6597334a92ea
Nguyen Toan <toanndm@gmail.com>
2024-03-02 13:56:50 +0700

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  products: IProduct[] = [];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://e-shop-dpxc.onrender.com/api/products').subscribe(
+      {
+        next: (response: any) => {
+          this.products = response.data;
+          console.log(this.products);
+        },
+        error: (e: any) => {
+          console.error(e);
+        },
+        complete: () => {} 
+      }
+    );
+  }
   title = 'e-shop-client';
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-hover-gray": "#C89F94",
+      },
+      fontFamily: {
+        Karla: ["Karla", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
